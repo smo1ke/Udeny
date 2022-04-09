b293e32097245861e0a2bdc145c99108950184a6
smo1ke <study.starevskiy@gmail.com>
2022-04-09 20:57:20 +0300

    First commit

---
+++ JS/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let number = 5;
+const leftBorderWidth = 1;
+
+
+// const result = confirm("Are you here?");
+
+// console.log('result :>> ', result);
+
+// const answer = prompt("Are you 18?", "18")
+// console.log('answer :>> ',typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt("What is your name?", "");
+answers[1] = prompt("What is your surname?", "");
+answers[2] = prompt("How old are your?", "");
+
+document.write(answers);
