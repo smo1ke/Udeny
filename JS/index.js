4e95f5018a66271fcaba146c9559677705b76301
smo1ke <study.starevskiy@gmail.com>
2022-04-10 18:10:31 +0300

    Second commit

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,54 @@
-'use strict'
+"use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '')
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '')
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// }
+
+// const a = prompt("Один из последних просмотренный фильмов?", ''),
+//       b = prompt("На сколько оцените его?",''),
+//       c = prompt("Один из последних просмотренный фильмов?",''),
+//       d = prompt("На сколько оцените его?",'')
+
+// personalMovieDB.movies[a] = b
+// personalMovieDB.movies[c] = d
+
+// console.log(personalMovieDB)
+
+if (4) {
+  console.log("Ok!");
+} else {
+  console.log("Error");
 }
 
-const a = prompt("Один из последних просмотренный фильмов?", ''),
-      b = prompt("На сколько оцените его?",''),
-      c = prompt("Один из последних просмотренный фильмов?",''),
-      d = prompt("На сколько оцените его?",'')
+// if (num < 49) {
+//     console.log("Error");
+// } else if (num > 100) {
+//     console.log("To much");
+// } else {
+//     console.log("Ok!");
+// }
 
+// num === 50 ? console.log("Ok!") : console.log("Error");
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
+// const num = 49;
 
-console.log(personalMovieDB)
-
+// switch (num) {
+//   case 49:
+//     console.log("Wrong");
+//     break;
+//   case 100:
+//     console.log("Wrong");
+//     break;
+//   case 50:
+//     console.log("Ok!");
+//     break;
+//   default:
+//     console.log("Not this time");
+//     break;
+// }
