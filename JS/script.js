2d8ed46f1913ac5bd774d9336f75caa9c2a7c991
smo1ke <study.starevskiy@gmail.com>
2022-04-11 21:02:49 +0300

    Sixh commit

--- JS/script.js
+++ JS/script.js
@@ -39,22 +39,22 @@
 // }
 // console.log(counter);
 
-const arr = [1, 21, 35, 4, 5, 6];
+// const arr = [1, 21, 35, 4, 5, 6];
 
-arr.sort(compareNum);
-console.log(arr);
-
-function compareNum(a, b) {
-  return a - b;
-}
-
-// arr[99] = 0;
-// console.log(arr.length);
+// arr.sort(compareNum);
 // console.log(arr);
 
-arr.forEach(function (item, i, arr) {
-  console.log(`${i}: ${item} in ${arr}`);
-});
+// function compareNum(a, b) {
+//   return a - b;
+// }
+
+// // arr[99] = 0;
+// // console.log(arr.length);
+// // console.log(arr);
+
+// arr.forEach(function (item, i, arr) {
+//   console.log(`${i}: ${item} in ${arr}`);
+// });
 
 // arr.pop();
 // arr.push(10);
@@ -64,12 +64,99 @@ arr.forEach(function (item, i, arr) {
 //   console.log(arr[i]);
 // }
 
-for (let value of arr) {
-  console.log(value);
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// const str = prompt("=>", "");
+
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join("; "));
+
+const obj = {
+  a: 5,
+  b: 1,
+};
+
+// const copy = obj;// Link
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+  let objCopy = {};
+
+  let key;
+  for (key in mainObj) {
+    objCopy[key] = mainObj;
+  }
+
+  return objCopy;
 }
 
-const str = prompt("=>", "");
+// const numbers = {
+//   a: 2,
+//   b: 5,
+//   c: {
+//     x: 7,
+//     y: 4,
+//   },
+// };
 
-const products = str.split(", ");
-products.sort();
-console.log(products.join("; "));
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+
+// console.log(newNumbers);
+
+const add = {
+  d: 17,
+  e: 20,
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ["a", "b", "c"];
+
+const newArray = oldArray.slice();
+
+newArray[1] = "asasasaa";
+
+console.log(newArray);
+console.log(oldArray);
+
+const video = ["youtube", "vimeo", "insta"],
+  blogs = ["wordpress", "livej", "blogger"],
+  internet = [...video, ...blogs, "facebook", "tiktok"];
+
+console.log(internet);
+
+function log(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ["a", "b"];
+
+const newAarray = [...array];
+
+const q = {
+  one: 1,
+  two: 2,
+};
+
+const newQ = { ...q };
+
+console.log(newQ);
