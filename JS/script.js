76a22e3ffea72e512e10de78e3f146b99f572e2e
smo1ke <study.starevskiy@gmail.com>
2022-04-11 20:20:24 +0300

    Fourth commit

--- JS/script.js
+++ JS/script.js
@@ -38,3 +38,38 @@
 //   }
 // }
 // console.log(counter);
+
+const arr = [1, 21, 35, 4, 5, 6];
+
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+  return a - b;
+}
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+arr.forEach(function (item, i, arr) {
+  console.log(`${i}: ${item} in ${arr}`);
+});
+
+// arr.pop();
+// arr.push(10);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+for (let value of arr) {
+  console.log(value);
+}
+
+const str = prompt("=>", "");
+
+const products = str.split(", ");
+products.sort();
+console.log(products.join("; "));
